e097ab023eb190293d61e77077da966e05f0d9d8
Kevin Tran <k.tran10@yahoo.com>
2023-11-26 21:41:07 -0600

    adding innerHTML test

--- main.js
+++ main.js
@@ -7,9 +7,26 @@ form.addEventListener('submit', (searchPokemonEvent) => {
     pokemonData(pokemonInputValue)
 })
 
-
+// https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/
 const pokemonData = async (pokemonInfo) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/{pokemonInfo}`)
     const data = await response.json()
     console.log(data)
+}
+
+function showPokemonInfo(data) {
+    let table = 
+    `<tr>
+        <th>Name</th>
+        <th>Image Sprite</th>
+    </tr>`;
+
+    for (let info of data.list) {
+        table += 
+    `<tr>
+        <td>${info.name}</td>
+        <td>${info.sprite}</td>
+    </tr>`
+    }
+    document.getElementById("pokemon").innerHTML = table
 }
