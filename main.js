ab1e82dcb00ce1d1bae3353e761209bd6191b84b
Kevin Tran <k.tran10@yahoo.com>
2023-11-26 21:59:44 -0600

    showing info in console, now need to make it only grab name/sprites

--- main.js
+++ main.js
@@ -2,31 +2,31 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', (searchPokemonEvent) => {
     searchPokemonEvent.preventDefault()
-    const pokemonInputValue = form[0]
-    console.log(pokemonInputValue.value)
-    pokemonData(pokemonInputValue)
+    const pokemonValue = form[0].value
+    console.log(pokemonValue.value)
+    pokemonData(pokemonValue)
 })
 
 // https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/
 const pokemonData = async (pokemonInfo) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/{pokemonInfo}`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInfo}`)
     const data = await response.json()
     console.log(data)
 }
 
-function showPokemonInfo(data) {
-    let table = 
-    `<tr>
-        <th>Name</th>
-        <th>Image Sprite</th>
-    </tr>`;
+// function showPokemonInfo(data) {
+//     let table = 
+//     `<tr>
+//         <th>Name</th>
+//         <th>Image Sprite</th>
+//     </tr>`;
 
-    for (let info of data.list) {
-        table += 
-    `<tr>
-        <td>${info.name}</td>
-        <td>${info.sprite}</td>
-    </tr>`
-    }
-    document.getElementById("pokemon").innerHTML = table
-}
+//     for (let info of data.list) {
+//         table += 
+//     `<tr>
+//         <td>${info.name}</td>
+//         <td>${info.sprite}</td>
+//     </tr>`
+//     }
+//     document.getElementById("pokemon").innerHTML = table
+// }
