b280db20f68264ce249f063d4fbd3ac28d37e6e3
Kevin Tran <k.tran10@yahoo.com>
2023-11-27 16:57:39 -0600

    added .textContent and moved code around

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 // test js file to html
 console.log('testing')
 
-// set up database, *refer to class notes*
+
+// DOM
+const pokemonNameInfo = document.getElementsByClassName('pokemonName');
+    document.body.append(pokemonNameInfo)
+    const pokemonSprite = document.getElementsByClassName('pokmeonSprite');
+    document.body.append(pokemonSprite)
+    const pokemonAbility = document.getElementsByClassName('pokemonAbility')
+    document.body.append(pokemonAbility)
+
+
+// use .addEventListener, *refer to class notes*
 const form = document.querySelector('form')
 form.addEventListener('submit', (searchPokemonEvent) => {
     searchPokemonEvent.preventDefault()
@@ -11,20 +21,18 @@ form.addEventListener('submit', (searchPokemonEvent) => {
     pokemonData(pokemonValue)
 })
 
-
+//grabbing data using .addEventListener()
 const pokemonData = async (pokemonInfo) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInfo}`)
     const data = await response.json()
     console.log(data)
    
+    pokemonNameInfo.textContent = data['name'];
+    pokemonSprite.textContent = data['sprites'][4];
+    pokemonAbility.textContent = data['ability'][0];
     
-    const pokemonNameInfo = document.getElementsByClassName('pokemonName');
-    document.body.append(pokemonNameInfo)
-    const pokemonSprite = document.getElementsByClassName('pokmeonSprite');
-    document.body.append(pokemonSprite)
-    const pokemonAbility = document.getElementsByClassName('pokemonAbility')
-    document.body.append(pokemonAbility)
 }
+
 
 
 // tried using this link for some guidance https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/
