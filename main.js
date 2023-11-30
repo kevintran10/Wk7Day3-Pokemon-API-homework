646b8dfb7f58f583d94d4c48395c2a128e6dbd3c
Kevin Tran <k.tran10@yahoo.com>
2023-11-29 23:07:39 -0600

    fixed so it's pulling data, added abilities, but only works on scyther...?

--- main.js
+++ main.js
@@ -2,38 +2,47 @@
 console.log('testing')
 
 
-// DOM
-const pokemonNameInfo = document.getElementsByClassName('pokemonName');
-    document.body.append(pokemonNameInfo)
-    const pokemonSprite = document.getElementsByClassName('pokmeonSprite');
-    document.body.append(pokemonSprite)
-    const pokemonAbility = document.getElementsByClassName('pokemonAbility')
-    document.body.append(pokemonAbility)
 
-
-// use .addEventListener, *refer to class notes*
-const form = document.querySelector('form')
-form.addEventListener('submit', (searchPokemonEvent) => {
-    searchPokemonEvent.preventDefault()
-    const pokemonValue = form[0].value
-    console.log(pokemonValue.value)
-    form[0].value = ''
-    pokemonData(pokemonValue)
-})
 
 //grabbing data using .addEventListener()
-const pokemonData = async (pokemonInfo) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInfo}`)
-    const data = await response.json()
-    console.log(data)
-   
-    pokemonNameInfo.textContent = data['name'];
-    pokemonSprite.textContent = data['sprites'][4];
-    pokemonAbility.textContent = data['ability'][0];
+const pokemonData = async () => {
+    const pokemonInfo = document.getElementById("pokemonName").value.toLowerCase()
+
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInfo}`)
+        const data = await response.json()
+        console.log(data)
+
+        const pokeName = data.name;
+        const pokeSprite = data.sprites.front_shiny;
+        const pokeSprite2 = data.sprites.back_default;
+        const pokeAbility = data.abilities[0].ability.name;
+        const pokeAbility2 = data.abilities[1].ability.name;
+        const pokeAbility3 = data.abilities[2].ability.name;
+
+        const pokeDisplay = document.getElementById("pokemon-display");
+        pokeDisplay.innerHTML = `
+        <h3>...it's!... ${pokeName.toUpperCase()}</h3>
+        <img src="${pokeSprite}" id="sprite-text">
+        <img src="${pokeSprite2}" id="sprite-text">
+        <div>Ability 1: ${pokeAbility.toUpperCase()}</div>
+        <div>Ability 2: ${pokeAbility2.toUpperCase()}</div>
+        <div>Ability 3: ${pokeAbility3.toUpperCase()}</div>
+        `;
+    } catch (error) {
+        console.error("Pokemon doesn't exist:", error)
+    }
+}  
+
+
+
+
+
+
+
+// pokemonSprite.textContent = data['sprites'][4];
+// pokemonAbility.textContent = data['ability'][0];
     
-}
-
-
 
 // tried using this link for some guidance https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/
 // function showPokemonInfo(data) {
